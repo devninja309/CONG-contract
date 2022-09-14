c70562e906da40f908e53d51ff3f414543960520
devninja309 <ryooyama.dev@gmail.com>
2022-09-14 00:22:09 -0500

    first commit

---
+++ contracts/CONG.sol
@@ -0,0 +1,49 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CONG is ERC20, Ownable {
+    mapping(address => bool) investors;
+    uint256 public preSaleStart;
+
+    constructor(string memory name_, string memory symbol_, uint256 totalSupply_) ERC20(name_, symbol_) {
+        _mint(msg.sender, totalSupply_);
+    }
+
+    function issue(address target, uint256 amount) external onlyOwner {
+        _mint(target, amount);
+    }
+
+    function enrollOne(address addr) external onlyOwner {
+        investors[addr] = true;
+    }
+
+    function enrollAll(address[] memory addr) external onlyOwner {
+        for(uint256 idx = 0; idx < addr.length; idx++) {
+            investors[addr[idx]] = true;
+        }
+    }
+
+    function setPreSaleStart(uint256 startTime) external onlyOwner {
+        preSaleStart = startTime;
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        super.transfer(to, amount);
+        if(investors[msg.sender]) {
+            uint256 interval = block.timestamp - preSaleStart;
+            if(interval >= 365 days && interval < 730 days) {
+                _burn(to, amount * 1 / 200);
+            }
+            if(interval >= 730 days) {
+                _burn(to, amount * 3 / 400);
+            }
+        } else {
+            _burn(to, amount * 1 / 100);
+        }
+        
+        return true;
+    }
+}
